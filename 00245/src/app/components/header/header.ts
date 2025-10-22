871bb42fae922cce523f645339f55c174a435663
RodrigoFreitas <riguigo@gmail.com>
2025-10-22 22:40:16 +0100

    commit: component hoje

--- 00245/src/app/components/header/header.ts
+++ 00245/src/app/components/header/header.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonEngine } from '@angular/ssr/node';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
--- 00245/src/app/components/main/main.ts
+++ 00245/src/app/components/main/main.ts
@@ -1,12 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
-export class Main {
+export class Main implements OnInit {
+
+  /* Global */
+  aluno: string = "Rodrigo Freitas";
+
+
+  ngOnInit(): void {
+    this.definirProblemas()
+
+  }
+
+
+  definirProblemas():void {
+    this.aluno;
+    let aluno: string = "Miguel";
+    console.log("this.aluno ", this.aluno);
+    console.log("this.aluno ", aluno);
+
+
+    
+
+
+  }
+
+
 
 }
