e7ed5f8f87719ee9da66cff949af897e2e6c03ff
RodrigoFreitas <riguigo@gmail.com>
2025-10-22 21:34:54 +0100

    header, main and footer

--- 00245/src/app/components/main/main.ts
+++ 00245/src/app/components/main/main.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
