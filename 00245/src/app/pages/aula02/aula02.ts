67686d346c7a392c7634854e45e390329c2d204c
RodrigoFreitas <riguigo@gmail.com>
2025-10-29 18:53:14 +0000

    Exercicios Aula02

--- 00245/src/app/pages/aula02/aula02.ts
+++ 00245/src/app/pages/aula02/aula02.ts
@@ -12,7 +12,7 @@ export class Aula02 implements OnInit{
     this.exemploEstruturasDecisao();
     this.exemploSwitch();
     this.ternario();
-    this.exerciciosIfeSwitch()
+    this.exercicios();
   }
 
   // ==========================================================
@@ -124,22 +124,11 @@ export class Aula02 implements OnInit{
  
   ternario = () => {
     let nota: number = 85;
-    let classificacao = nota >= 90 ? 'A' : nota >= 80 ? 'B' : nota >= 70 ? 'C' : nota >= 50 ? 'D' : 'F';
+    let classificacao = nota >= 90 ? 'A' : 'B' ;
  
     console.log('Classificação: ' + classificacao);
-  }
+  };
  
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -201,28 +190,204 @@ export class Aula02 implements OnInit{
   //                  Exercicios
   // ==========================================================
 
-  exerciciosIfeSwitch(): void {
+
+
+exercicios = () => {
+    console.log('--- Exercícios: Estruturas de Decisão If, Else IF e Else---');
+
+    console.log(
+      '1️ Faça um programa que receba uma nota (0–100) e exiba "Aprovado" se for >= 60, caso contrário "Reprovado".'
+    );
+
+    let nota: number = 55;
+
+    if (nota >= 60) {
+      console.log("Aprovado");
+    } else {
+      console.log("Reprovado")
+    }
+
+
+
+
+    console.log(
+      '2️ Crie um sistema que exiba mensagens diferentes conforme a faixa etária (criança, adolescente, adulto, idoso).'
+    );
+
+    let idade: number = 20;
+
+    if (idade >= 65) {
+      console.log("Idoso");
+    } else if (idade >= 18) {
+      console.log("Adulto");
+    } else if (idade >= 12) {
+      console.log ("Adolescente");
+    } else {
+      console.log("Criança")
+    }
+
+    console.log(
+      '3️ Peça uma temperatura e mostre se está "frio", "agradável" ou "quente" usando operadores ternários.'
+    );
+
+    let tempgraus: number = 19;
+    let tempatual = tempgraus <= 15 ? 'Frio' : tempgraus <= 25 ? 'Agradável' : 'Quente' ;
+    console.log('Temperatura Atual: ' + tempatual);
+
+ 
+    console.log('--- Exercícios: Estrutura Switch ---');
+ 
+    console.log(
+      '4 Peça um número de 1 a 7 e exiba o dia da semana correspondente.'
+    );
+
+    let diaDaSemana: number = 5;
     
-  }
+    switch (diaDaSemana) {
+      case 1:
+        console.log('O Dia da semana é Segunda-feira');
+      break;
+      case 2:
+        console.log('O Dia da semana é Terça-feira');
+      break;
+      case 3:
+        console.log('O Dia da semana é Quarta-feira');
+      break;
+      case 4:
+        console.log('O Dia da semana é Quinta-feira');
+      break;
+      case 5:
+        console.log('O Dia da semana é Sexta-feira');
+      break;
+      case 6:
+        console.log('O Dia da semana é Sábado');
+      break;
+      case 7:
+        console.log('O Dia da semana é Domingo');
+      break;
+      default:
+         console.log('O Numero inserido não corresponde a nenhum dia da semana');
+      }
+        
+
+
+    console.log(
+      '5 Crie um menu de operações matemáticas (somar, subtrair, multiplicar, dividir) com switch.'
+    );
+
+    let operacao: string = 'multiplicar';
+    let resultado: number;
+ 
+    switch (operacao) {
+      case 'somar':
+        resultado = 10 + 10;
+        break;
+      case 'subtrair':
+        resultado = 10 - 10;
+        break;
+      case 'multiplicar':
+        resultado = 10 * 10;
+        break;
+      case 'dividir':
+        resultado = 10 / 10;
+        break;
+      default:
+        resultado = 0;
+    }
+    console.log('Resultado:', resultado);
 
 
 
 
 
+    console.log(
+      '6 Use um switch para classificar uma cor (vermelho, verde, azul, outro).'
+    );
+
+    // Não entendi muito bem este Exercicio, não como fazer mas sim a logica.
+
+    let cor: string = "azul";
+
+    switch (cor) {
+      case "vermelho":
+        console.log("A cor é vermelha.");
+        break;
+
+      case "verde":
+        console.log("A cor é verde.");
+        break;
+
+      case "azul":
+        console.log("A cor é azul.");
+        break;
+
+      default:
+        console.log("Cor não reconhecida.");
+        break;
+    }
+    
+
+ 
+    console.log('🎯 INICIANDO EXERCÍCIOS SOMENTE COM OPERADOR TERNÁRIO SIMPLES\n');
+    console.log(
+      '🎯  EXERCÍCIO 1: Verificador de Maioridade, DESCRIÇÃO: Verifica se uma pessoa é maior de idade usando ternário simples\n'
+    );
+
+    let idade2: number = 17;
+    let resultado2: string = idade2 >= 18 ? "maior de idade." : "menor de idade.";
+    console.log('É ', resultado2);
 
 
 
+    console.log(
+      '\n🔢 EXERCÍCIO 2: Verificador de Número Par/Ímpar, DESCRIÇÃO: Verifica se um número é par ou ímpar usando ternário simples, USO: Operação matemática dentro da condição'
+    );
+ 
+    let num: number = 12;
+
+    let parimpar: string = num % 2 === 0 ? "O número é par." : "O número é ímpar.";
+    console.log(parimpar);
 
 
+    console.log(
+      '\n🔐 EXERCÍCIO 3: Verificador de Login.  DESCRIÇÃO: Verifica se o usuário está logado e retorna mensagem apropriada   USO: Condição com valores booleanos'
+    );
+
+    let logado: boolean = true;
+
+    let respostaLogin: string = logado ? "Utilizador está logado." : "Utilizador está deslogado.";
+
+    console.log(respostaLogin);
 
 
-
-
-
-
-
-
-
-
-
+  };
 }
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
